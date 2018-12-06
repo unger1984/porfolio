e82c01b731aac169faaef19886dd21baf28e1df5
unger1984 <unger1984@gmail.com>
2018-12-06 14:22:31 +0300

    GumsNet

--- portfolio.php
+++ portfolio.php
@@ -8,153 +8,187 @@
 $PageTitle = "Portfolio";
 include_once 'includes/header.php'
 ?>
-    <div class="banner-wrapper">
-        <h2 style="text-align: center;">Mobile development</h2>
-        <table class="bordered striped">
-                    <tr>
-                        <td width="300px">
-                            <a href="/files/posteldelux.png" class="fancybox">
-                                <img src="/files/posteldelux.png" width="300px" height="300px"/>
-                            </a>
-                        </td>
-                        <td>
-                            <h3>Postel Deluxe</h3>
-                            <span class="created">created 05.12.2018</span>
-                            <p>Приложение для получения скидок по домашнему текстилю</p>
-                            <p>Реализовано на React Native.</p>
-                            <p><b>Опубликовано:</b>
-                                <a href="https://play.google.com/store/apps/details?id=com.awcoding.posteldeluxe" target="_blank">Google Play Market</a>
-                            </p>
-                        </td>
-                    </tr>
-            <tr>
-                <td width="300px">
-                    <a href="/files/fl.png" class="fancybox">
-                        <img src="/files/fl.png" width="300px" height="300px"/>
-                    </a>
-                </td>
-                <td>
-                    <h3>FL.RU</h3>
-                    <span class="created">created 29.03.2018</span>
-                    <p>Неофициальный клиент популярной фриланс биржи fl.ru</p>
-                    <p>Реализовано на React Native. Серверная часть на NodeJS.</p>
-                    <p><b>Опубликовано:</b>
-                        <a href="https://play.google.com/store/apps/details?id=com.awcoding.fl" target="_blank">Google Play Market</a>
-                    </p>
-                </td>
-            </tr>
-            <tr>
-                <td width="300px">
-                    <a href="/files/memsreader.png" class="fancybox">
-                        <img src="/files/memsreader.png" width="300px" height="300px"/>
-                    </a>
-                </td>
-                <td>
-                    <h3>Mems Reader</h3>
-                    <span class="created">created 04.03.2018</span>
-                    <p>Развлекательное приложение позволяющее тебе скрасить время просмотром различных приколов и юморных картинок</p>
-                    <p>Реализовано на React Native.</p>
-                    <p><b>Опубликовано:</b>
-                        <a href="https://play.google.com/store/apps/details?id=com.awcoding.memsreader" target="_blank">Google Play Market</a>
-                    </p>
-                </td>
-            </tr>
-            <tr>
-                <td width="300px">
-                    <a href="/files/bashim.png" class="fancybox">
-                        <img src="/files/bashim.png" width="300px" height="300px"/>
-                    </a>
-                </td>
-                <td>
-                    <h3>BashIm</h3>
-                    <span class="created">created 03.03.2018</span>
-                    <p>Читалка самого известного цитатника рунета</p>
-                    <p>Реализовано на React Native.</p>
-                    <p><b>Опубликовано:</b>
-                        <a href="https://play.google.com/store/apps/details?id=com.awcoding.bashim" target="_blank">Google Play Market</a>
-                    </p>
-                </td>
-            </tr>
-            <tr>
-                <td width="300px">
-                    <a href="/files/thelistings.png" class="fancybox">
-                        <img src="/files/thelistings.png" width="300px" height="300px"/>
-                    </a>
-                </td>
-                <td>
-                    <h3>TheListings</h3>
-                    <span class="created">created 30.10.2017</span>
-                    <p>Trade online without any fees on The Listings New Zealand</p>
-                    <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне заказчика.</p>
-                    <p><b>Опубликовано:</b>
-                        <a href="https://play.google.com/store/apps/details?id=com.awcoding.thelistings" target="_blank">Google Play Market</a>
-                    </p>
-                </td>
-            </tr>
-            <tr>
-                <td width="300px">
-                    <a href="/files/endure.png" class="fancybox">
-                        <img src="/files/endure.png" width="300px" height="300px"/>
-                    </a>
-                </td>
-                <td>
-                    <h3>Endure</h3>
-                    <span class="created">created 12.05.2017</span>
-                    <p>Train and Practice your kegel muscle (pelvic floor muscle).
-                        Pelvic floor exercises can be effective in treating premature ejaculation in men who have had lifelong problems</p>
-                    <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне заказчика.</p>
-                    <p><b>Опубликовано:</b>
-                        <a href="https://play.google.com/store/apps/details?id=com.awcoding.endure" target="_blank">Google Play Market</a>
-                    </p>
-                </td>
-            </tr>
-            <tr>
-                <td width="300px">
-                    <a href="/files/bcmcalculator.png" class="fancybox">
-                        <img src="/files/bcmcalculator.png" width="300px" height="300px"/>
-                    </a>
-                </td>
-                <td>
-                    <h3>BCM Calculator</h3>
-                    <span class="created">created 17.04.2017</span>
-                    <p>Nissan/Infinity BCM Out-code to PIN-code converter Tool</p>
-                    <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне заказчика.</p>
-                    <p><b>Опубликовано:</b>
-                        <a href="https://play.google.com/store/apps/details?id=com.awcoding.bcmcalculator" target="_blank">Google Play Market</a>,
-                        <a href="https://itunes.apple.com/us/app/bcm-calculator/id1222909233?l=ru&ls=1&mt=8" target="_blank">App Store</a>
-                    </p>
-                </td>
-            </tr>
-            <tr>
-                <td width="300px">
-                    <a href="/files/bronevik.png" class="fancybox">
-                        <img src="/files/bronevik.png" width="300px" height="300px"/>
-                    </a>
-                </td>
-                <td>
-                    <h3>Броневик</h3>
-                    <span class="created">created 04.02.2017</span>
-                    <p>Запись на услуги по временным слотам.</p>
-                    <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне заказчика.</p>
-                    <p><b>Опубликовано:</b> <a href="https://play.google.com/store/apps/details?id=com.awcoding.assistant" target="_blank">Google Play Market</a></p>
-                </td>
-            </tr>
-            <tr>
-                <td width="300px">
-                    <a href="/files/krasaten.png" class="fancybox">
-                        <img src="/files/krasaten.png" width="300px" height="300px"/>
-                    </a>
-                </td>
-                <td>
-                    <h3>Рецепты и рецептики</h3>
-                    <span class="created">created 06.02.2017</span>
-                    <p>Кулинарное приложение с простыми и вкусными рецептами на каждый день или к праздничному столу.</p>
-                    <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне заказчика.</p>
-                    <p><b>Опубликовано:</b> <a href="https://play.google.com/store/apps/details?id=com.awcoding.krasaten" target="_blank">Google Play Market</a></p>
-                </td>
-            </tr>
-        </table>
-    </div>
+<div class="banner-wrapper">
+    <h2 style="text-align: center;">Mobile development</h2>
+    <table class="bordered striped">
+        <tr>
+            <td width="300px">
+                <a href="/files/gumsnet.png" class="fancybox">
+                    <img src="/files/gumsnet.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>GumsNet</h3>
+                <span class="created">created 06.12.2018</span>
+                <p>Личный кабинет интернет провайдера</p>
+                <p>Реализовано на React Native. Вместо API на сервере приложение работает с html кодом сайта</p>
+                <p><b>Опубликовано:</b>
+                    <a href="https://play.google.com/store/apps/details?id=com.awcoding.gumsnet" target="_blank">Google
+                        Play Market</a>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/posteldelux.png" class="fancybox">
+                    <img src="/files/posteldelux.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>Postel Deluxe</h3>
+                <span class="created">created 05.12.2018</span>
+                <p>Приложение для получения скидок по домашнему текстилю</p>
+                <p>Реализовано на React Native.</p>
+                <p><b>Опубликовано:</b>
+                    <a href="https://play.google.com/store/apps/details?id=com.awcoding.posteldeluxe"
+                       target="_blank">Google Play Market</a>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/fl.png" class="fancybox">
+                    <img src="/files/fl.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>FL.RU</h3>
+                <span class="created">created 29.03.2018</span>
+                <p>Неофициальный клиент популярной фриланс биржи fl.ru</p>
+                <p>Реализовано на React Native. Серверная часть на NodeJS.</p>
+                <p><b>Опубликовано:</b>
+                    <a href="https://play.google.com/store/apps/details?id=com.awcoding.fl" target="_blank">Google Play
+                        Market</a>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/memsreader.png" class="fancybox">
+                    <img src="/files/memsreader.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>Mems Reader</h3>
+                <span class="created">created 04.03.2018</span>
+                <p>Развлекательное приложение позволяющее тебе скрасить время просмотром различных приколов и юморных
+                    картинок</p>
+                <p>Реализовано на React Native.</p>
+                <p><b>Опубликовано:</b>
+                    <a href="https://play.google.com/store/apps/details?id=com.awcoding.memsreader" target="_blank">Google
+                        Play Market</a>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/bashim.png" class="fancybox">
+                    <img src="/files/bashim.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>BashIm</h3>
+                <span class="created">created 03.03.2018</span>
+                <p>Читалка самого известного цитатника рунета</p>
+                <p>Реализовано на React Native.</p>
+                <p><b>Опубликовано:</b>
+                    <a href="https://play.google.com/store/apps/details?id=com.awcoding.bashim" target="_blank">Google
+                        Play Market</a>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/thelistings.png" class="fancybox">
+                    <img src="/files/thelistings.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>TheListings</h3>
+                <span class="created">created 30.10.2017</span>
+                <p>Trade online without any fees on The Listings New Zealand</p>
+                <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне
+                    заказчика.</p>
+                <p><b>Опубликовано:</b>
+                    <a href="https://play.google.com/store/apps/details?id=com.awcoding.thelistings" target="_blank">Google
+                        Play Market</a>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/endure.png" class="fancybox">
+                    <img src="/files/endure.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>Endure</h3>
+                <span class="created">created 12.05.2017</span>
+                <p>Train and Practice your kegel muscle (pelvic floor muscle).
+                    Pelvic floor exercises can be effective in treating premature ejaculation in men who have had
+                    lifelong problems</p>
+                <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне
+                    заказчика.</p>
+                <p><b>Опубликовано:</b>
+                    <a href="https://play.google.com/store/apps/details?id=com.awcoding.endure" target="_blank">Google
+                        Play Market</a>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/bcmcalculator.png" class="fancybox">
+                    <img src="/files/bcmcalculator.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>BCM Calculator</h3>
+                <span class="created">created 17.04.2017</span>
+                <p>Nissan/Infinity BCM Out-code to PIN-code converter Tool</p>
+                <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне
+                    заказчика.</p>
+                <p><b>Опубликовано:</b>
+                    <a href="https://play.google.com/store/apps/details?id=com.awcoding.bcmcalculator" target="_blank">Google
+                        Play Market</a>,
+                    <a href="https://itunes.apple.com/us/app/bcm-calculator/id1222909233?l=ru&ls=1&mt=8"
+                       target="_blank">App Store</a>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/bronevik.png" class="fancybox">
+                    <img src="/files/bronevik.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>Броневик</h3>
+                <span class="created">created 04.02.2017</span>
+                <p>Запись на услуги по временным слотам.</p>
+                <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне
+                    заказчика.</p>
+                <p><b>Опубликовано:</b> <a href="https://play.google.com/store/apps/details?id=com.awcoding.assistant"
+                                           target="_blank">Google Play Market</a></p>
+            </td>
+        </tr>
+        <tr>
+            <td width="300px">
+                <a href="/files/krasaten.png" class="fancybox">
+                    <img src="/files/krasaten.png" width="300px" height="300px"/>
+                </a>
+            </td>
+            <td>
+                <h3>Рецепты и рецептики</h3>
+                <span class="created">created 06.02.2017</span>
+                <p>Кулинарное приложение с простыми и вкусными рецептами на каждый день или к праздничному столу.</p>
+                <p>Реализовано на React Native. Мной написано только мобильное приложение. Сервер реализован на стороне
+                    заказчика.</p>
+                <p><b>Опубликовано:</b> <a href="https://play.google.com/store/apps/details?id=com.awcoding.krasaten"
+                                           target="_blank">Google Play Market</a></p>
+            </td>
+        </tr>
+    </table>
+</div>
 <?php
 include_once 'includes/footer.php'
 ?>
