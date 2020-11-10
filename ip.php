d469eaec1980ffd222bceb19eca502ba27f95420
Unger Andrey <unger1984@gmail.com>
2020-11-10 13:32:47 +0300

    change to html

--- ip.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: cobalt
- * Date: 01.12.2017
- * Time: 23:50
- */
-print($_SERVER['REMOTE_ADDR']);
-?>
--- vk.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: cobalt
- * Date: 14.11.2017
- * Time: 15:34
- */
-print_r($_GET);
-?>
